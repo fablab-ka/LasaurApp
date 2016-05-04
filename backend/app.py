ad7a4279d301f45af84a17c5867f2d4c82624c51
Sven Hecht <sven.hecht@citrix.com>
2016-05-04 20:17:57 +0200

    fixed time function

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,8 @@ else:
 
 def pauseIfCardNotAvailable():
     if config["use_id_card_access_restriction"]:
-        if time.time() - lastCardCheck > cardCheckInterval:
+        if (time.time() - lastCardCheck) > cardCheckInterval:
+            lastCardCheck = time.time()
             if not has_valid_id():
                 SerialManager.set_pause(True)
 
