a0c3c03bd2d52a276c3bdd927b3913cfea114ff5
morbidick <mail@morbi-happens.de>
2016-05-13 00:28:55 +0200

    set max upload file size to 5mb

--- backend/bottle.py
+++ backend/bottle.py
@@ -999,7 +999,7 @@ class BaseRequest(object):
     __slots__ = ('environ')
 
     #: Maximum size of memory buffer for :attr:`body` in bytes.
-    MEMFILE_MAX = 102400
+    MEMFILE_MAX = 5*1024*1024
 
     def __init__(self, environ=None):
         """ Wrap a WSGI environ dictionary. """
