869622c8fb9367c35a04515e9c36eb34625de348
Philip Caroli <philip@caroli.de>
2017-05-14 15:15:56 +0200

    Removed debug alert()

--- backend/odooHelper.py
+++ backend/odooHelper.py
@@ -7,6 +7,8 @@ import json
 
 class OdooHelper:
 
+
+
     def __init__(self, username, password, url, db):
         self.username = username
         self.password = password
@@ -15,19 +17,17 @@ class OdooHelper:
 
         #Open Session with Odoo
         self.session = requests.session()
-        req1 = self.session.get(self.url + "/web")
+        req1 = self.session.get(self.url + "/web?db=" + self.db)
         self.csrf_token = re.findall(r'name="csrf_token" value="(.+?)"', req1.content)
         payload = {
-            "login": "admin@admin.de",
-            "password": "admin",
+            "login": self.username,
+            "password": self.password,
             "csrf_token": self.csrf_token,
         }
-        self.session.post(self.url + "/web/login", data=payload)
+        req2 = self.session.post(self.url + "/web/login", data=payload)
 
-        #test if authentification is successful
-        #req2 = self.session.get(self.url + "/web")
         #TODO find way of checking if /web/login or /web get reached
-        #print(req2.content)
+
 
     def callAPI(self, path, args=None):
         req = self.session.post(self.url + path, data={"csrf_token": self.csrf_token})
