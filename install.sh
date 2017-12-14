92da6c658d0a088228bd08b675f71c2ee9ca98e4
Philip Caroli <philip@caroli.de>
2017-12-14 22:34:49 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt-get install -y python3-pip
+pip3 install -r requirements.txt
+
+echo "[Unit]
+Description=LaserSaurApp
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory="$PWD"
+ExecStart= "$PWD"/run.sh
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/lasaurapp.service
+
+systemctl enable lasaurapp.service
+systemctl start lasaurapp.service
+
+echo "installation finished"
